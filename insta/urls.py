b11b42801e7dc5eeb6e59599b16f41355c1033f4
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-06-09 11:23:11 +0300

    add url pattens for rest framework

--- insta/urls.py
+++ insta/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('instagram.urls'))
+    path('', include('Instagram.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 if settings.DEBUG:
